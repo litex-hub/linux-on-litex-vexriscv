litex-hub/linux-on-litex-vexriscv

commit f0168c146b304ed8487a2c947b4aa1c00461b78a

make: adjust integrated_rom_size and set sys_clk_freq to 64MHz on OrangeCrab.

make.py
@@ -344,8 +344,11 @@ def main():
 
         # SoC parameters (and override for boards that don't support default parameters) -----------
         soc_kwargs = {}
-        soc_kwargs.update(integrated_rom_size=0x10000)
-        if board_name in ["de0nano", "orangecrab"]:
+        soc_kwargs.update(integrated_rom_size=0xa000)
+        if board_name in ["de0nano"]:
+            soc_kwargs.update(l2_size=2048) # Not enough blockrams for default l2_size of 8192
+        if board_name in ["orangecrab"]:
+            soc_kwargs.update(sys_clk_freq=64e6)
             soc_kwargs.update(l2_size=2048) # Not enough blockrams for default l2_size of 8192
         if board_name in ["kc705"]:
             soc_kwargs.update(uart_baudrate=500e3) # Set UART baudrate to 500KBauds since 1Mbauds not supported

commit 2636987a21adcd21788c6a82e962375170d74a93

make: OrangeCrab: Fix sys_clk_freq
When building for OrangeCrab:

    $ ./make.py --board=orangecrab --build
    ...
    Traceback (most recent call last):
      File "./make.py", line 492, in <module>
	main()
      File "./make.py", line 431, in main
	soc = SoCLinux(board.soc_cls, **soc_kwargs)
      File "/path/to/linux-on-litex-vexriscv/soc_linux.py", line 283, in SoCLinux
	return _SoCLinux(**kwargs)
      File "/path/to/linux-on-litex-vexriscv/soc_linux.py", line 111, in __init__
	**kwargs)
      File "/path/to/litex/litex-boards/litex_boards/targets/orangecrab.py", line 166, in __init__
	self.submodules.crg = crg_cls(platform, sys_clk_freq, with_usb_pll)
      File "/path/to/litex/litex-boards/litex_boards/targets/orangecrab.py", line 137, in __init__
	reset_timer = WaitTimer(sys_clk_freq)
      File "/path/to/litex/migen/migen/genlib/misc.py", line 83, in __init__
	count = Signal(bits_for(t), reset=t)
      File "/path/to/litex/migen/migen/fhdl/bitcontainer.py", line 18, in bits_for
	r = log2_int(n + 1, False)
      File "/path/to/litex/migen/migen/fhdl/bitcontainer.py", line 10, in log2_int
	r = (n - 1).bit_length()
    AttributeError: 'float' object has no attribute 'bit_length'

Fix this by converting the clock frequency to an integer before
assigning it to sys_clk_freq, like most targets in the litex_boards
repository (incl. orangecrab.py) do.

Fixes: f0168c146b304ed8 ("make: adjust integrated_rom_size and set sys_clk_freq to 64MHz on OrangeCrab.")
Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>

make.py
@@ -262,7 +262,7 @@ class HADBadge(Board):
 
 class OrangeCrab(Board):
     soc_kwargs = {
-        "sys_clk_freq": 64e6,          # Increase sys_clk_freq to 64MHz (48MHz default).
+        "sys_clk_freq": int(64e6),     # Increase sys_clk_freq to 64MHz (48MHz default).
         "l2_size":      2048,          # Reduce l2_size (Not enough blockrams).
         "integrated_rom_size": 0xa000, # Reduce integrated_rom_size.
     }