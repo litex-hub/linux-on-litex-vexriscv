4c855dd8638d3b5e30f2df71f682da4ca6724751
Florent Kermarrec <florent@enjoy-digital.fr>
2022-03-17 16:33:21 +0100

    sim: Cleanup and remove Ethernet support (not really useful).

--- sim.py
+++ sim.py
@@ -35,21 +35,12 @@ from litex.tools.litex_json2dts_linux import generate_dts
 # IOs ----------------------------------------------------------------------------------------------
 
 _io = [
+    # Clk / Rst.
     ("sys_clk", 0, Pins(1)),
     ("sys_rst", 0, Pins(1)),
-    ("serial", 0,
-        Subsignal("source_valid", Pins(1)),
-        Subsignal("source_ready", Pins(1)),
-        Subsignal("source_data",  Pins(8)),
 
-        Subsignal("sink_valid", Pins(1)),
-        Subsignal("sink_ready", Pins(1)),
-        Subsignal("sink_data",  Pins(8)),
-    ),
-    ("eth_clocks", 0,
-        Subsignal("none", Pins()),
-    ),
-    ("eth", 0,
+    # Serial.
+    ("serial", 0,
         Subsignal("source_valid", Pins(1)),
         Subsignal("source_ready", Pins(1)),
         Subsignal("source_data",  Pins(8)),
@@ -70,8 +61,8 @@ class Platform(SimPlatform):
 
 class Supervisor(Module, AutoCSR):
     def __init__(self):
-        self._finish  = CSR()  # controlled from CPU
-        self.finish = Signal() # controlled from logic
+        self._finish = CSR()    # Controlled from CPU.
+        self.finish  = Signal() # Controlled from logic.
         self.sync += If(self._finish.re | self.finish, Finish())
 
 # SoCLinux -----------------------------------------------------------------------------------------
@@ -81,8 +72,7 @@ class SoCLinux(SoCCore):
         init_memories    = False,
         sdram_module     = "MT48LC16M16",
         sdram_data_width = 32,
-        sdram_verbosity  = 0,
-        with_ethernet    = False):
+        sdram_verbosity  = 0):
         platform     = Platform()
         sys_clk_freq = int(100e6)
 
@@ -97,7 +87,7 @@ class SoCLinux(SoCCore):
         SoCCore.__init__(self, platform, clk_freq=sys_clk_freq,
             cpu_type                 = "vexriscv_smp",
             cpu_variant              = "linux",
-            integrated_rom_size      = 0x8000,
+            integrated_rom_size      = 0x10000,
             uart_name                = "sim")
         self.add_config("DISABLE_DELAYS")
 
@@ -129,18 +119,6 @@ class SoCLinux(SoCCore):
             l2_cache_size = 0)
         self.add_constant("SDRAM_TEST_DISABLE") # Skip SDRAM test to avoid corrupting pre-initialized contents.
 
-        # Ethernet ---------------------------------------------------------------------------------
-        if with_ethernet:
-            # eth phy
-            self.submodules.ethphy = LiteEthPHYModel(self.platform.request("eth", 0))
-            # eth mac
-            ethmac = LiteEthMAC(phy=self.ethphy, dw=32,
-                interface="wishbone", endianness=self.cpu.endianness)
-            self.submodules.ethmac = ethmac
-            self.add_memory_region("ethmac", self.mem_map["ethmac"], 0x2000, type="io")
-            self.add_wb_slave(self.mem_map["ethmac"], self.ethmac.bus)
-            self.add_interrupt("ethmac")
-
     def generate_dts(self, board_name):
         json_src = os.path.join("build", board_name, "csr.json")
         dts = os.path.join("build", board_name, "{}.dts".format(board_name))
@@ -161,9 +139,6 @@ def main():
     parser.add_argument("--sdram-module",     default="MT48LC16M16",   help="Select SDRAM chip.")
     parser.add_argument("--sdram-data-width", default=32,              help="Set SDRAM chip data width.")
     parser.add_argument("--sdram-verbosity",  default=0,               help="Set SDRAM checker verbosity.")
-    parser.add_argument("--with-ethernet",    action="store_true",     help="enable Ethernet support.")
-    parser.add_argument("--local-ip",         default="192.168.1.50",  help="Local IP address of SoC (default=192.168.1.50).")
-    parser.add_argument("--remote-ip",        default="192.168.1.100", help="Remote IP address of TFTP server (default=192.168.1.100).")
     VexRiscvSMP.args_fill(parser)
     verilator_build_args(parser)
     args = parser.parse_args()
@@ -172,20 +147,14 @@ def main():
     verilator_build_kwargs = verilator_build_argdict(args)
     sim_config = SimConfig(default_clk="sys_clk")
     sim_config.add_module("serial2console", "serial")
-    if args.with_ethernet:
-        sim_config.add_module("ethernet", "eth", args={"interface": "tap0", "ip": args.remote_ip})
 
     for i in range(2):
-        soc = SoCLinux( i!=0,
+        soc = SoCLinux(
+            init_memories    = i!=0,
             sdram_module     = args.sdram_module,
             sdram_data_width = int(args.sdram_data_width),
-            sdram_verbosity  = int(args.sdram_verbosity),
-            with_ethernet    = args.with_ethernet)
-        if args.with_ethernet:
-            for i in range(4):
-                soc.add_constant("LOCALIP{}".format(i+1), int(args.local_ip.split(".")[i]))
-            for i in range(4):
-                soc.add_constant("REMOTEIP{}".format(i+1), int(args.remote_ip.split(".")[i]))
+            sdram_verbosity  = int(args.sdram_verbosity)
+        )
         board_name = "sim"
         build_dir  = os.path.join("build", board_name)
         builder = Builder(soc, output_dir=build_dir,
@@ -198,7 +167,6 @@ def main():
         if i == 0:
             soc.generate_dts(board_name)
             soc.compile_dts(board_name)
-
 
 if __name__ == "__main__":
     main()
